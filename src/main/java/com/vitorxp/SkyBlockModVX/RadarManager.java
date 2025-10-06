65fe814194da2eae45a3742cbe782ab16a56bf6e
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2025-10-06 09:32:14 -0300

    Update!

--- src/main/java/com/vitorxp/SkyBlockModVX/RadarManager.java
+++
@@ -1,9 +0,0 @@
-package com.vitorxp.SkyBlockModVX;
-
-import java.awt.Rectangle;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
-public class RadarManager {
-    public static final Map<String, Rectangle> playerClickAreas = new ConcurrentHashMap<>();
-}
