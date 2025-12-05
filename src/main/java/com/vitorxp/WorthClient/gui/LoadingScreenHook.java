14393871a92cb143b78911fb0b6c88d139e62f4f
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2025-12-05 09:05:46 -0300

    Update LoadingScreenHook.java

--- src/main/java/com/vitorxp/WorthClient/gui/LoadingScreenHook.java
+++ src/main/java/com/vitorxp/WorthClient/gui/LoadingScreenHook.java
@@ -1,37 +1,41 @@
 package com.vitorxp.WorthClient.gui;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiDownloadTerrain;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import java.lang.reflect.Field;
-
 public class LoadingScreenHook {
 
-    private static VXLoadingScreenRenderer customRenderer;
+    public static LoadingScreenHook INSTANCE = new LoadingScreenHook();
+    public static WorthLoadingGUI customGUI;
+
+    private boolean forced = false;
 
     public static void inject() {
-        try {
-            Minecraft mc = Minecraft.getMinecraft();
-
-            if (customRenderer == null)
-                customRenderer = new VXLoadingScreenRenderer(mc);
-
-            Field loadingField = Minecraft.class.getDeclaredField("loadingScreen");
-            loadingField.setAccessible(true);
-
-            VXLoadingScreenOverride override = new VXLoadingScreenOverride(mc, customRenderer);
-            loadingField.set(mc, override);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        if (customGUI == null)
+            customGUI = new WorthLoadingGUI(Minecraft.getMinecraft());
     }
 
     @SubscribeEvent
-    public void onGuiOpen(GuiOpenEvent e) {
-        if (e.gui instanceof GuiDownloadTerrain || e.gui == null)
-            inject();
+    public void onGuiOpen(GuiOpenEvent event) {
+
+        if (event.gui instanceof GuiDownloadTerrain) {
+
+            if (customGUI == null)
+                customGUI = new WorthLoadingGUI(Minecraft.getMinecraft());
+
+            event.setCanceled(true);
+            Minecraft.getMinecraft().displayGuiScreen(customGUI);
+
+            forced = true;
+            return;
+        }
+
+        if (forced && event.gui == null) {
+            event.setCanceled(true);
+            forced = false;
+        }
     }
 }
