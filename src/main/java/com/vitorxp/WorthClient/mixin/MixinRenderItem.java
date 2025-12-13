a0219ea49b1b3d6becc8d1055e1494dd1a61abc9
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2025-12-12 21:14:01 -0300

    Create MixinRenderItem.java

---
+++ src/main/java/com/vitorxp/WorthClient/mixin/MixinRenderItem.java
@@ -0,0 +1,20 @@
+package com.vitorxp.WorthClient.mixin;
+
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.resources.model.IBakedModel;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RenderItem.class)
+public class MixinRenderItem {
+
+    @Inject(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/resources/model/IBakedModel;)V", at = @At("HEAD"), cancellable = true)
+    public void limitItemRendering(ItemStack stack, IBakedModel model, CallbackInfo ci) {
+        if (stack == null || stack.getItem() == null) {
+            ci.cancel();
+        }
+    }
+}
