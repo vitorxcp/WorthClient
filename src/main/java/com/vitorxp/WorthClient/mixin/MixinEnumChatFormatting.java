11e037c4d59c39469450ed58133e20928517fc1f
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2025-12-12 21:13:46 -0300

    Create MixinEnumChatFormatting.java

---
+++ src/main/java/com/vitorxp/WorthClient/mixin/MixinEnumChatFormatting.java
@@ -0,0 +1,22 @@
+package com.vitorxp.WorthClient.mixin;
+
+import net.minecraft.util.EnumChatFormatting;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.regex.Pattern;
+
+@Mixin(EnumChatFormatting.class)
+public class MixinEnumChatFormatting {
+
+    private static final Pattern STRIP_COLOR_PATTERN = Pattern.compile("(?i)" + String.valueOf('\u00a7') + "[0-9A-FK-OR]");
+
+    @Overwrite
+    public static String getTextWithoutFormattingCodes(String text) {
+        return text == null ? null : STRIP_COLOR_PATTERN.matcher(text).replaceAll("");
+    }
+
+    @Shadow
+    public String toString() { return null; }
+}
