b825a0135c6bc28b59e2d0c7e212fa0d0f9711ef
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2025-12-18 16:43:44 -0300

    Update MixinRendererLivingEntity.java

--- src/main/java/com/vitorxp/WorthClient/mixin/MixinRendererLivingEntity.java
+++ src/main/java/com/vitorxp/WorthClient/mixin/MixinRendererLivingEntity.java
@@ -9,10 +9,11 @@ import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.ResourceLocation;
-import org.lwjgl.opengl.GL11; // <--- Importante para glNormal3f
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -33,7 +34,7 @@ public abstract class MixinRendererLivingEntity<T extends EntityLivingBase> {
     public void onRenderName(T entity, double x, double y, double z, CallbackInfo ci) {
         if (!this.canRenderName(entity)) return;
 
-        if (entity instanceof EntityPlayer) {
+        if (entity instanceof EntityPlayer || entity instanceof EntityArmorStand) {
             String originalText = entity.getDisplayName().getFormattedText();
             String cleanText = cleanString(originalText);
 
@@ -49,54 +50,69 @@ public abstract class MixinRendererLivingEntity<T extends EntityLivingBase> {
         FontRenderer fontrenderer = mc.fontRendererObj;
         float f = 1.6F;
         float f1 = 0.016666668F * f;
+
         GlStateManager.pushMatrix();
-        GlStateManager.translate((float)x + 0.0F, (float)y + entityIn.height + 0.5F, (float)z);
+        GlStateManager.translate((float)x, (float)y + entityIn.height + 0.5F, (float)z);
         GL11.glNormal3f(0.0F, 1.0F, 0.0F);
         GlStateManager.rotate(-mc.getRenderManager().playerViewY, 0.0F, 1.0F, 0.0F);
         GlStateManager.rotate(mc.getRenderManager().playerViewX, 1.0F, 0.0F, 0.0F);
         GlStateManager.scale(-f1, -f1, f1);
+
         GlStateManager.disableLighting();
         GlStateManager.depthMask(false);
-        GlStateManager.disableDepth();
         GlStateManager.enableBlend();
         GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-        Tessellator tessellator = Tessellator.getInstance();
-        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+
         int strWidth = fontrenderer.getStringWidth(str);
         int halfWidth = (strWidth + 12) / 2;
-        GlStateManager.disableTexture2D();
-        worldrenderer.begin(7, DefaultVertexFormats.POSITION_COLOR);
-        worldrenderer.pos(-halfWidth - 1, -1, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-        worldrenderer.pos(-halfWidth - 1, 8, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-        worldrenderer.pos(halfWidth + 1, 8, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-        worldrenderer.pos(halfWidth + 1, -1, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
-        tessellator.draw();
+
+        GlStateManager.disableDepth();
+
+        drawBackgroundRect(halfWidth, 0.1F);
+
         GlStateManager.enableTexture2D();
-        GlStateManager.enableAlpha();
-        float alpha = entityIn.isSneaking() ? 0.3F : 1.0F;
-        GlStateManager.color(1.0F, 1.0F, 1.0F, alpha);
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 0.2F);
         mc.getTextureManager().bindTexture(WORTH_ICON);
         drawIcon(-halfWidth, -1, 8, 8);
-        int textX = -halfWidth + 11;
-        fontrenderer.drawString(str, textX, 0, 553648127);
+        fontrenderer.drawString(str, -halfWidth + 11, 0, 553648127);
+
         GlStateManager.enableDepth();
-        fontrenderer.drawString(str, textX, 0, -1);
-        GlStateManager.bindTexture(0);
+        GlStateManager.depthMask(true);
+
+        drawBackgroundRect(halfWidth, 0.25F);
+
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        mc.getTextureManager().bindTexture(WORTH_ICON);
+        drawIcon(-halfWidth, -1, 8, 8);
+        fontrenderer.drawString(str, -halfWidth + 11, 0, -1);
+
         GlStateManager.enableLighting();
         GlStateManager.disableBlend();
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        GlStateManager.depthMask(true);
         GlStateManager.popMatrix();
+    }
+
+    private void drawBackgroundRect(int halfWidth, float alpha) {
+        Tessellator tessellator = Tessellator.getInstance();
+        WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+        GlStateManager.disableTexture2D();
+        worldrenderer.begin(7, DefaultVertexFormats.POSITION_COLOR);
+        worldrenderer.pos(-halfWidth - 1, -1, 0.0D).color(0.0F, 0.0F, 0.0F, alpha).endVertex();
+        worldrenderer.pos(-halfWidth - 1, 8, 0.0D).color(0.0F, 0.0F, 0.0F, alpha).endVertex();
+        worldrenderer.pos(halfWidth + 1, 8, 0.0D).color(0.0F, 0.0F, 0.0F, alpha).endVertex();
+        worldrenderer.pos(halfWidth + 1, -1, 0.0D).color(0.0F, 0.0F, 0.0F, alpha).endVertex();
+        tessellator.draw();
+        GlStateManager.enableTexture2D();
     }
 
     private void drawIcon(int x, int y, int width, int height) {
         Tessellator tessellator = Tessellator.getInstance();
         WorldRenderer worldrenderer = tessellator.getWorldRenderer();
         worldrenderer.begin(7, DefaultVertexFormats.POSITION_TEX);
-        worldrenderer.pos(x, y + height, -0.01D).tex(0.0D, 1.0D).endVertex();
-        worldrenderer.pos(x + width, y + height, -0.01D).tex(1.0D, 1.0D).endVertex();
-        worldrenderer.pos(x + width, y, -0.01D).tex(1.0D, 0.0D).endVertex();
-        worldrenderer.pos(x, y, -0.01D).tex(0.0D, 0.0D).endVertex();
+        worldrenderer.pos(x, y + height, 0.0D).tex(0.0D, 1.0D).endVertex();
+        worldrenderer.pos(x + width, y + height, 0.0D).tex(1.0D, 1.0D).endVertex();
+        worldrenderer.pos(x + width, y, 0.0D).tex(1.0D, 0.0D).endVertex();
+        worldrenderer.pos(x, y, 0.0D).tex(0.0D, 0.0D).endVertex();
         tessellator.draw();
     }
 
