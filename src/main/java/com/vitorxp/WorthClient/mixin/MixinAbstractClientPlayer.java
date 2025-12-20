43766575a06d8ad8756321d0026d6b125fb18883
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2025-12-20 12:19:01 -0300

    Update MixinAbstractClientPlayer.java

--- src/main/java/com/vitorxp/WorthClient/mixin/MixinAbstractClientPlayer.java
+++ src/main/java/com/vitorxp/WorthClient/mixin/MixinAbstractClientPlayer.java
@@ -2,8 +2,8 @@ package com.vitorxp.WorthClient.mixin;
 
 import com.mojang.authlib.GameProfile;
 import com.vitorxp.WorthClient.socket.ClientSocket;
+import com.vitorxp.WorthClient.utils.AnimatedCape;
 import com.vitorxp.WorthClient.utils.CapeLoader;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.network.NetworkPlayerInfo;
 import net.minecraft.entity.player.EntityPlayer;
@@ -16,6 +16,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
+import java.util.Set;
 
 @Mixin(AbstractClientPlayer.class)
 public abstract class MixinAbstractClientPlayer extends EntityPlayer {
@@ -23,21 +24,26 @@ public abstract class MixinAbstractClientPlayer extends EntityPlayer {
     public MixinAbstractClientPlayer(World worldIn, GameProfile gameProfileIn) {
         super(worldIn, gameProfileIn);
     }
-
     @Shadow @Nullable protected abstract NetworkPlayerInfo getPlayerInfo();
-
     @Inject(method = "getLocationCape", at = @At("HEAD"), cancellable = true)
     public void getLocationCape(CallbackInfoReturnable<ResourceLocation> cir) {
         if (this.getGameProfile() == null || this.getGameProfile().getName() == null) return;
+        String playerName = this.getGameProfile().getName().toLowerCase();
+        if (ClientSocket.playerCosmetics.containsKey(playerName)) {
+            Set<String> cosmetics = ClientSocket.playerCosmetics.get(playerName);
 
-        String playerName = this.getGameProfile().getName();
-
-        if (ClientSocket.hasCosmetic(playerName, "cape_free_redeworth")) {
-
-            ResourceLocation worth_cape_free = CapeLoader.loadCape("cape_free_redeworth");
-
-            if (worth_cape_free != null) {
-                cir.setReturnValue(worth_cape_free);
+            for (String cosmeticId : cosmetics) {
+                AnimatedCape animCape = CapeLoader.getAnimatedCape(cosmeticId);
+                if (animCape != null) {
+                    animCape.update();
+                    cir.setReturnValue(animCape.getTextureLocation());
+                    return;
+                }
+                ResourceLocation staticCape = CapeLoader.loadCape(cosmeticId);
+                if (staticCape != null) {
+                    cir.setReturnValue(staticCape);
+                    return;
+                }
             }
         }
     }
