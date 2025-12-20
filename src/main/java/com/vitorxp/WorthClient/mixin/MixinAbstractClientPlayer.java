61a158d69586280fc374919bc0e6c176a80d8b81
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2025-12-20 19:39:21 -0300

    Update MixinAbstractClientPlayer.java

--- src/main/java/com/vitorxp/WorthClient/mixin/MixinAbstractClientPlayer.java
+++ src/main/java/com/vitorxp/WorthClient/mixin/MixinAbstractClientPlayer.java
@@ -1,49 +1,32 @@
 package com.vitorxp.WorthClient.mixin;
 
-import com.mojang.authlib.GameProfile;
-import com.vitorxp.WorthClient.socket.ClientSocket;
-import com.vitorxp.WorthClient.utils.AnimatedCape;
-import com.vitorxp.WorthClient.utils.CapeLoader;
+import com.vitorxp.WorthClient.utils.CapeSimulationHolder;
+import com.vitorxp.WorthClient.utils.StickSimulation;
 import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraft.client.network.NetworkPlayerInfo;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import javax.annotation.Nullable;
-import java.util.Set;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractClientPlayer.class)
-public abstract class MixinAbstractClientPlayer extends EntityPlayer {
-
-    public MixinAbstractClientPlayer(World worldIn, GameProfile gameProfileIn) {
+public abstract class MixinAbstractClientPlayer extends EntityPlayer implements CapeSimulationHolder {
+    public MixinAbstractClientPlayer(World worldIn, com.mojang.authlib.GameProfile gameProfileIn) {
         super(worldIn, gameProfileIn);
     }
-    @Shadow @Nullable protected abstract NetworkPlayerInfo getPlayerInfo();
-    @Inject(method = "getLocationCape", at = @At("HEAD"), cancellable = true)
-    public void getLocationCape(CallbackInfoReturnable<ResourceLocation> cir) {
-        if (this.getGameProfile() == null || this.getGameProfile().getName() == null) return;
-        String playerName = this.getGameProfile().getName().toLowerCase();
-        if (ClientSocket.playerCosmetics.containsKey(playerName)) {
-            Set<String> cosmetics = ClientSocket.playerCosmetics.get(playerName);
-
-            for (String cosmeticId : cosmetics) {
-                AnimatedCape animCape = CapeLoader.getAnimatedCape(cosmeticId);
-                if (animCape != null) {
-                    animCape.update();
-                    cir.setReturnValue(animCape.getTextureLocation());
-                    return;
-                }
-                ResourceLocation staticCape = CapeLoader.loadCape(cosmeticId);
-                if (staticCape != null) {
-                    cir.setReturnValue(staticCape);
-                    return;
-                }
+    @Unique
+    private final StickSimulation simulation = new StickSimulation();
+    @Override
+    public StickSimulation getSimulation() {
+        return this.simulation;
+    }
+    @Inject(method = "<init>", at = @At("RETURN"))
+    public void init(World worldIn, com.mojang.authlib.GameProfile playerProfile, CallbackInfo ci) {
+        if (this.simulation.points.isEmpty()) {
+            for (int i = 0; i < 16; i++) {
+                this.simulation.points.add(new StickSimulation.Point());
             }
         }
     }
