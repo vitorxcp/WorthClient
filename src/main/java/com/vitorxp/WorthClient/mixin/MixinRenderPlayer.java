fb8971ede0173a87c14d865b0941995a9367505b
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2026-01-02 13:04:26 -0300

    Create MixinRenderPlayer.java

---
+++ src/main/java/com/vitorxp/WorthClient/mixin/MixinRenderPlayer.java
@@ -0,0 +1,27 @@
+package com.vitorxp.WorthClient.mixin;
+
+import com.vitorxp.WorthClient.utils.Skin3DLayer;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.entity.RendererLivingEntity; // CORRIGIDO: Nome da classe na 1.8.9
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.RenderPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(RenderPlayer.class)
+public abstract class MixinRenderPlayer extends RendererLivingEntity<AbstractClientPlayer> { // CORRIGIDO
+
+    public MixinRenderPlayer(RenderManager renderManagerIn, ModelBase modelBaseIn, float shadowSizeIn) {
+        super(renderManagerIn, modelBaseIn, shadowSizeIn);
+    }
+
+    @Inject(method = "<init>(Lnet/minecraft/client/renderer/entity/RenderManager;Z)V", at = @At("RETURN"))
+    public void onInit(RenderManager renderManager, boolean useSmallArms, CallbackInfo ci) {
+        RenderPlayer renderPlayer = (RenderPlayer) (Object) this;
+
+        this.addLayer(new Skin3DLayer(renderPlayer));
+    }
+}
