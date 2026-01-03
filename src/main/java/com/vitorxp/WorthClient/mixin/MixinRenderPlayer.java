3095e85b19ca373232a43a4cbfc722b20b37920f
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2026-01-02 21:53:29 -0300

    Update MixinRenderPlayer.java

--- src/main/java/com/vitorxp/WorthClient/mixin/MixinRenderPlayer.java
+++ src/main/java/com/vitorxp/WorthClient/mixin/MixinRenderPlayer.java
@@ -3,7 +3,7 @@ package com.vitorxp.WorthClient.mixin;
 import com.vitorxp.WorthClient.utils.Skin3DLayer;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.renderer.entity.RendererLivingEntity; // CORRIGIDO: Nome da classe na 1.8.9
+import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.RenderPlayer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderPlayer.class)
-public abstract class MixinRenderPlayer extends RendererLivingEntity<AbstractClientPlayer> { // CORRIGIDO
+public abstract class MixinRenderPlayer extends RendererLivingEntity<AbstractClientPlayer> {
 
     public MixinRenderPlayer(RenderManager renderManagerIn, ModelBase modelBaseIn, float shadowSizeIn) {
         super(renderManagerIn, modelBaseIn, shadowSizeIn);
