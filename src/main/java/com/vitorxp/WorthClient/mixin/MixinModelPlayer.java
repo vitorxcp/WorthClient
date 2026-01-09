6bf82e724df40660aa412fe7e971fbe6093f51e6
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2026-01-08 22:38:44 -0300

    Update MixinModelPlayer.java

--- src/main/java/com/vitorxp/WorthClient/mixin/MixinModelPlayer.java
+++ src/main/java/com/vitorxp/WorthClient/mixin/MixinModelPlayer.java
@@ -1,33 +1,45 @@
 package com.vitorxp.WorthClient.mixin;
 
-import com.vitorxp.WorthClient.utils.PerspectiveMod;
-import net.minecraft.client.Minecraft;
+import com.vitorxp.WorthClient.WorthClient;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.model.ModelPlayer;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ModelPlayer.class)
-public class MixinModelPlayer {
+public class MixinModelPlayer extends ModelBiped {
 
-    @Inject(method = "setRotationAngles", at = @At("HEAD"))
+    @Inject(method = "setRotationAngles", at = @At("RETURN"))
     public void onSetRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entityIn, CallbackInfo ci) {
-        if (PerspectiveMod.perspectiveToggled && entityIn == Minecraft.getMinecraft().thePlayer) {
-            float yawDiff = PerspectiveMod.cameraYaw - entityIn.rotationYaw;
+        if (entityIn instanceof AbstractClientPlayer) {
 
-            while (yawDiff >= 180.0F) yawDiff -= 360.0F;
-            while (yawDiff < -180.0F) yawDiff += 360.0F;
-
-            float clampedYaw = MathHelper.clamp_float(yawDiff, -90.0F, 90.0F);
-
-            ModelPlayer model = (ModelPlayer) (Object) this;
-            model.bipedHead.rotateAngleY = clampedYaw * 0.017453292F;
-            model.bipedHead.rotateAngleX = PerspectiveMod.cameraPitch * 0.017453292F;
-            model.bipedHeadwear.rotateAngleY = model.bipedHead.rotateAngleY;
-            model.bipedHeadwear.rotateAngleX = model.bipedHead.rotateAngleX;
+            if (WorthClient.skin3D) {
+                this.bipedHeadwear.showModel = false;
+                try {
+                    ModelPlayer mp = (ModelPlayer) (Object) this;
+                    mp.bipedBodyWear.showModel = false;
+                    mp.bipedLeftArmwear.showModel = false;
+                    mp.bipedRightArmwear.showModel = false;
+                    mp.bipedLeftLegwear.showModel = false;
+                    mp.bipedRightLegwear.showModel = false;
+                } catch (Exception e) {
+                }
+            } else {
+                this.bipedHeadwear.showModel = true;
+                try {
+                    ModelPlayer mp = (ModelPlayer) (Object) this;
+                    mp.bipedBodyWear.showModel = true;
+                    mp.bipedLeftArmwear.showModel = true;
+                    mp.bipedRightArmwear.showModel = true;
+                    mp.bipedLeftLegwear.showModel = true;
+                    mp.bipedRightLegwear.showModel = true;
+                } catch (Exception e) {
+                }
+            }
         }
     }
 }
