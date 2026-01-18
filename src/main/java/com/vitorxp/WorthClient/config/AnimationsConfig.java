11d6411ebb47f84e9452634a6b6234c8d07fffa9
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2026-01-18 12:31:55 -0300

    Create AnimationsConfig.java

---
+++ src/main/java/com/vitorxp/WorthClient/config/AnimationsConfig.java
@@ -0,0 +1,19 @@
+package com.vitorxp.WorthClient.config;
+
+    public class AnimationsConfig {
+    public static boolean enabled = true;
+    public static boolean blockHit17 = true;
+    public static boolean smoothSwing = true;
+    public static boolean oldRod = true;
+    public static boolean oldBow = true;
+    public static boolean oldSneak = true;
+    public static boolean itemTransforms = true;
+    public static boolean damageShake = true;
+    public static boolean healthFlash = true;
+    public static boolean userItemWhileDigging = true;
+    public static boolean alwaysSwing = true;
+    public static float itemPosX = 0.0f;
+    public static float itemPosY = 0.0f;
+    public static float itemPosZ = 0.0f;
+    public static float itemScale = 1.0f;
+}
