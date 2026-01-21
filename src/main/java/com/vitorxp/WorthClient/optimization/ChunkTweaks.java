d3237273d2bb75822aa2ff53be9873edec68c0ce
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2026-01-21 12:28:37 -0300

    Update ChunkTweaks.java

--- src/main/java/com/vitorxp/WorthClient/optimization/ChunkTweaks.java
+++ src/main/java/com/vitorxp/WorthClient/optimization/ChunkTweaks.java
@@ -5,29 +5,19 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-
 public class ChunkTweaks {
     private int ticker = 0;
-
+    private final Minecraft mc = Minecraft.getMinecraft();
 
     @SubscribeEvent
     public void onClientTick(TickEvent.ClientTickEvent e) {
-        if (!PerfConfig.autoRenderDistance) return;
         if (e.phase != TickEvent.Phase.END) return;
 
-        if (++ticker % 100 != 0) return;
-
-        Minecraft mc = Minecraft.getMinecraft();
-        if (mc.theWorld == null || mc.thePlayer == null) return;
-
-        int fps = Minecraft.getDebugFPS();
-        int current = mc.gameSettings.renderDistanceChunks;
-
-        int min = PerfConfig.minRenderDistance;
-        int target = PerfConfig.fpsTarget;
-
-        if (fps < target && current > min) {
-            //mc.gameSettings.renderDistanceChunks = current - 1;
+        if (++ticker > 6000) {
+            ticker = 0;
+            if (mc.thePlayer != null && PerfConfig.autoMemoryClean) {
+                System.runFinalization();
+            }
         }
     }
 }
