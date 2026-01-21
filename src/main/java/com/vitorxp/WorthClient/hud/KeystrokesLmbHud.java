58ba0fceeda9b2fccb40ea083988172ef1f17d99
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2026-01-20 21:25:05 -0300

    Update KeystrokesLmbHud.java

--- src/main/java/com/vitorxp/WorthClient/hud/KeystrokesLmbHud.java
+++ src/main/java/com/vitorxp/WorthClient/hud/KeystrokesLmbHud.java
@@ -1,25 +1,30 @@
 package com.vitorxp.WorthClient.hud;
 
+import com.vitorxp.WorthClient.config.KeystrokesSettings;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraft.client.renderer.GlStateManager;
 import org.lwjgl.input.Mouse;
 
 public class KeystrokesLmbHud extends HudElement {
 
-    public KeystrokesLmbHud() {
-        super("KeystrokesLMB", 10, 120);
-    }
+    public KeystrokesLmbHud() { super("KeystrokesLMB", 10, 120); }
 
     @Override
     public void render(RenderGameOverlayEvent event) {
-        if (!com.vitorxp.WorthClient.WorthClient.keystrokesOverlay) return;
+        if (!KeystrokesSettings.enabled || !KeystrokesSettings.showClicks) return;
 
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(this.x, this.y, 0);
+        GlStateManager.scale(KeystrokesSettings.scale, KeystrokesSettings.scale, 1);
+
+        int w = (int)(KeystrokesSettings.boxSize * 1.5 + 1);
+        int h = (int)KeystrokesSettings.boxSize;
         int cps = com.vitorxp.WorthClient.WorthClient.keystrokesManager.getCpsLeft();
-        KeystrokesDrawing.drawCpsKey("LMB", cps, this.x, this.y, Mouse.isButtonDown(0), getWidth(), getHeight());
+
+        KeystrokesDrawing.drawCps("LMB", cps, 0, 0, Mouse.isButtonDown(0), w, h);
+        GlStateManager.popMatrix();
     }
 
-    @Override
-    public int getWidth() { return 60; }
-
-    @Override
-    public int getHeight() { return 20; }
+    @Override public int getWidth() { return (int)((KeystrokesSettings.boxSize * 1.5 + 1) * KeystrokesSettings.scale); }
+    @Override public int getHeight() { return (int)(KeystrokesSettings.boxSize * KeystrokesSettings.scale); }
 }
