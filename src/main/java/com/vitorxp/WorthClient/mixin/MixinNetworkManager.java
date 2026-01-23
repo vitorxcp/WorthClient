f03162a946e9cb74975453ca2db47d852435c0cb
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2026-01-23 16:13:32 -0300

    Update MixinNetworkManager.java

--- src/main/java/com/vitorxp/WorthClient/mixin/MixinNetworkManager.java
+++ src/main/java/com/vitorxp/WorthClient/mixin/MixinNetworkManager.java
@@ -1,7 +1,14 @@
 package com.vitorxp.WorthClient.mixin;
 
+import com.viaversion.viaversion.api.connection.UserConnection;
+import de.florianmichael.viamcp.ViaMCP;
+import de.florianmichael.viamcp.MCPVLBPipeline;
+import de.florianmichael.vialoadingbase.ViaLoadingBase;
+import com.viaversion.viaversion.connection.UserConnectionImpl;
+import com.viaversion.viaversion.protocol.ProtocolPipelineImpl;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.socket.SocketChannelConfig;
+import io.netty.channel.socket.SocketChannel;
 import net.minecraft.network.NetworkManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,18 +19,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinNetworkManager {
 
     @Inject(method = "channelActive", at = @At("HEAD"))
-    public void onChannelActive(ChannelHandlerContext ctx, CallbackInfo ci) {
-        try {
-            if (ctx.channel().config() instanceof SocketChannelConfig) {
-                SocketChannelConfig config = (SocketChannelConfig) ctx.channel().config();
+    public void onChannelActive(ChannelHandlerContext context, CallbackInfo ci) {
+        Channel channel = context.channel();
 
-                config.setTcpNoDelay(true);
+        if (channel instanceof SocketChannel && ViaLoadingBase.getInstance().getTargetVersion().getVersion() != ViaMCP.NATIVE_VERSION) {
 
-                config.setTrafficClass(0x18);
+            final UserConnection user = new UserConnectionImpl(channel, true);
+            new ProtocolPipelineImpl(user);
 
-                config.setKeepAlive(true);
-            }
-        } catch (Exception ignored) {
+            context.pipeline().addLast(new MCPVLBPipeline(user));
+        }
+    }
+
+    @Inject(method = "setCompressionTreshold", at = @At("HEAD"), cancellable = true)
+    public void onSetCompressionTreshold(int treshold, CallbackInfo ci) {
+        if (ViaLoadingBase.getInstance().getTargetVersion().getVersion() > 47) {
+            ci.cancel();
         }
     }
 }
