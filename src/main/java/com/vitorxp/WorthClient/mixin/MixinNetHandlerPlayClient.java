8bf746c1bae0a167080fa34274f73e392602e447
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2026-01-24 11:12:34 -0300

    Update MixinNetHandlerPlayClient.java

--- src/main/java/com/vitorxp/WorthClient/mixin/MixinNetHandlerPlayClient.java
+++ src/main/java/com/vitorxp/WorthClient/mixin/MixinNetHandlerPlayClient.java
@@ -1,18 +1,24 @@
 package com.vitorxp.WorthClient.mixin;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.network.NetHandlerPlayClient;
 import net.minecraft.client.network.NetworkPlayerInfo;
-import net.minecraft.network.play.server.S29PacketSoundEffect; // <--- Importante
+import net.minecraft.network.play.server.*;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject; // <--- Importante
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo; // <--- Importante
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.UUID;
 
 @Mixin(NetHandlerPlayClient.class)
-public class MixinNetHandlerPlayClient {
+public abstract class MixinNetHandlerPlayClient {
+
+    @Shadow private Minecraft gameController;
+    @Shadow private WorldClient clientWorldController;
 
     @Redirect(method = "handleSpawnPlayer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/NetHandlerPlayClient;getPlayerInfo(Ljava/util/UUID;)Lnet/minecraft/client/network/NetworkPlayerInfo;"))
     public NetworkPlayerInfo onGetPlayerInfo(NetHandlerPlayClient instance, UUID uniqueId) {
@@ -27,6 +33,41 @@ public class MixinNetHandlerPlayClient {
     public void onHandleSoundEffect(S29PacketSoundEffect packet, CallbackInfo ci) {
         String name = packet.getSoundName();
         if (name != null && (name.equals("minecraft:damage.thorns") || name.equals("damage.thorns"))) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "handleScoreboardObjective", at = @At("HEAD"), cancellable = true)
+    private void onHandleScoreboardObjective(S3BPacketScoreboardObjective packetIn, CallbackInfo ci) {
+        if (this.clientWorldController == null) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "handleUpdateScore", at = @At("HEAD"), cancellable = true)
+    private void onHandleUpdateScore(S3CPacketUpdateScore packetIn, CallbackInfo ci) {
+        if (this.clientWorldController == null) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "handleDisplayScoreboard", at = @At("HEAD"), cancellable = true)
+    private void onHandleDisplayScoreboard(S3DPacketDisplayScoreboard packetIn, CallbackInfo ci) {
+        if (this.clientWorldController == null) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "handleTeams", at = @At("HEAD"), cancellable = true)
+    private void onHandleTeams(S3EPacketTeams packetIn, CallbackInfo ci) {
+        if (this.clientWorldController == null) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "handleWorldBorder", at = @At("HEAD"), cancellable = true)
+    private void onHandleWorldBorder(S44PacketWorldBorder packetIn, CallbackInfo ci) {
+        if (this.clientWorldController == null) {
             ci.cancel();
         }
     }
